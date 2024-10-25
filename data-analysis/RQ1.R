8c2bbf8b22a68b14e5903fc4b51ecc30bb07b7ab
BerryC-VU <2672919@student.vu.nl>
2024-10-26 00:25:22 +0200

    R analysis files

---
+++ data-analysis/RQ1.R
@@ -0,0 +1,108 @@
+# RQ1: energy consumption 
+
+library(ggplot2)
+library(dplyr)
+library(rstatix)
+library(effsize)
+
+data <- read.csv("run_table.csv")
+
+summary(data)
+
+# box plot: quantization type
+data$quantization_type <- trimws(data$quantization_type)
+
+data$quantization_type <- factor(data$quantization_type, levels = c("32-bit", "8-bit", "4-bit"))
+
+ggplot(data, aes(x = quantization_type, y = GPU.Energy, fill = quantization_type)) +
+  geom_boxplot() +
+  labs(title = "GPU Energy Usage by Quantization Type", 
+       x = "Quantization Type", 
+       y = "GPU Energy (J)") +
+  theme_minimal() +
+  theme(legend.position = "none", 
+        legend.title = element_text(size = 12),
+        legend.text = element_text(size = 8),
+        plot.title = element_text(hjust = 0.5, size = 14), # Title font size
+        axis.title.y = element_text(size = 11), # Y-axis title font size
+        axis.text.y = element_text(size = 11),
+        axis.title.x = element_text(size = 11), # Y-axis title font size
+        axis.text.x = element_text(size = 11)) 
+
+
+energy_stats <- data %>%
+  group_by(quantization_type) %>%
+  summarise(
+    mean_energy = mean(`GPU.Energy`, na.rm = TRUE),
+    std_energy = sd(`GPU.Energy`, na.rm = TRUE)
+  )
+
+energy_stats
+
+# bar plot: task+quantization type
+energy_summary <- data %>%
+  group_by(task_name, quantization_type) %>%
+  summarise(mean_energy = mean(GPU.Energy, na.rm = TRUE), .groups = 'drop')
+
+ggplot(energy_summary, aes(x = quantization_type, y = mean_energy, fill = quantization_type)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  geom_text(aes(label = sprintf("%.2f", mean_energy)), vjust = -0.5, position = position_dodge(width = 0.9)) +
+  facet_wrap(~ task_name) +
+  labs(title = "Comparison of GPU Energy Usage by Task and Quantization Type",
+       x = "Quantization Type",
+       y = "Mean GPU Energy (J)") +
+  theme_minimal() +
+  theme(legend.position = "bottom", 
+        legend.title = element_text(size = 12),
+        legend.text = element_text(size = 8),
+        plot.title = element_text(hjust = 0.5, size = 14), # Title font size
+        axis.title.y = element_text(size = 11), # Y-axis title font size
+        axis.text.y = element_text(size = 11),
+        axis.title.x = element_text(size = 11), # Y-axis title font size
+        axis.text.x = element_text(size = 11)) 
+
+
+# Normality Check
+shapiro_results <- data %>%
+  group_by(quantization_type) %>%
+  summarise(shapiro_stat = shapiro_test(GPU.Energy)$statistic,
+            shapiro_p_value = shapiro_test(GPU.Energy)$p.value)
+
+shapiro_results
+
+results <- data %>%
+  group_by(quantization_type) %>%
+  mutate(
+    log_GPU_Energy = log(GPU.Energy + 1),  # Log transformation to handle zero values
+    sqrt_GPU_Energy = sqrt(GPU.Energy)     # Square root transformation
+  ) %>%
+  group_by(quantization_type) %>% 
+  summarise(
+    log_normality_p_value = shapiro_test(log_GPU_Energy)$p.value,
+    sqrt_normality_p_value = shapiro_test(sqrt_GPU_Energy)$p.value
+  )
+
+results
+
+# Kruskal-Wallis Test
+kruskal_test_result <- kruskal.test(GPU.Energy ~ quantization_type, data = data)
+
+kruskal_test_result
+
+
+
+delta_32_8 <- cliff.delta(data$GPU.Energy[data$quantization_type == "32-bit"],
+                          data$GPU.Energy[data$quantization_type == "8-bit"])
+
+# Calculate Cliff's Delta for 32-bit vs 4-bit
+delta_32_4 <- cliff.delta(data$GPU.Energy[data$quantization_type == "32-bit"],
+                          data$GPU.Energy[data$quantization_type == "4-bit"])
+
+# Calculate Cliff's Delta for 8-bit vs 4-bit
+delta_8_4 <- cliff.delta(data$GPU.Energy[data$quantization_type == "8-bit"],
+                         data$GPU.Energy[data$quantization_type == "4-bit"])
+
+# Print the results
+print(list("32-bit vs 8-bit" = delta_32_8,
+           "32-bit vs 4-bit" = delta_32_4,
+           "8-bit vs 4-bit" = delta_8_4))
